328e0e5bfd7dd7ad0b357236d17e4235fad4c3ba
rajivojha <rajivojha@digitalindia.gov.in>
2022-09-10 03:15:48 +0530

    add run.py file

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from resources.item import Item, ItemList
@@ -7,7 +8,7 @@ from resources.store import Store, StoreList
 #from flask_jwt import JWT, jwt_required
 
 app= Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]= "sqlite://///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"]= os.environ.get("DATABASE_URL", "sqlite://///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= False
 api= Api(app)
 
